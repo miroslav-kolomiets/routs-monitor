349be68aa39fa6ea5da2d27bc6fa595195a28ad2
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2019-09-16 16:55:13 +0300

    Rewrite chart component

---
+++ src/components/GanttChart/data.js
@@ -0,0 +1,64 @@
+export const eventsList = [
+  {
+    task: 'desk12',
+    type: 'development',
+    startTime: '2013-2-1',
+    endTime: '2013-2-4',
+    details: 'No sketching either, really',
+  },
+
+  {
+    task: 'desk19',
+    type: 'development',
+    startTime: '2013-2-6',
+    endTime: '2013-2-9',
+    details: 'All the things',
+  },
+
+  {
+    task: 'desk10',
+    type: 'coding',
+    startTime: '2013-2-2',
+    endTime: '2013-2-6',
+    details: 'all three lines of it',
+  },
+
+  {
+    task: 'desk11',
+    type: 'coding',
+    startTime: '2013-2-6',
+    endTime: '2013-2-9',
+    details: 'All the things',
+  },
+
+  {
+    task: 'desk9',
+    type: 'promotion',
+    startTime: '2013-2-9',
+    endTime: '2013-2-12',
+    details: 'This counts, right?',
+  },
+
+  {
+    task: 'desk31',
+    type: 'promotion',
+    startTime: '2013-2-12',
+    endTime: '2013-2-14',
+    details: 'All the things',
+  },
+  {
+    task: 'desk23',
+    type: 'celebration',
+    startTime: '2013-2-8',
+    endTime: '2013-2-13',
+    details: 'All the things',
+  },
+
+  {
+    task: 'desk3',
+    type: 'celebration',
+    startTime: '2013-2-13',
+    endTime: '2013-2-16',
+    details: 'All the things',
+  },
+];
